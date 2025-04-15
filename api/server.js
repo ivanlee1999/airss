f5f09feaff545c24ac61ca3dc175f3fd2eb1d694
Yifan Li <ivan@Yifans-MacBook-Pro.local>
2025-04-14 23:52:20 -0700

    Add RSS feed subscription and publishing endpoints

--- api/server.js
+++ api/server.js
@@ -16,16 +16,105 @@ apiApp.get("/subscriptions", (req, res) => {
   res.json({ subscriptions });
 });
 
-apiApp.post("/subscribe", (req, res) => {
-  const { url } = req.body;
+import Parser from 'rss-parser';
+const rssParser = new Parser();
+
+apiApp.post("/subscribe", async (req, res) => {
+  const { url, name } = req.body;
   if (!url) {
     return res.status(400).json({ error: "Missing RSS feed URL" });
   }
-  subscribeToFeed(url);
-  res.json({ message: `Subscribed to ${url}` });
+  let feedName = name;
+  if (!feedName) {
+    try {
+      const feed = await rssParser.parseURL(url);
+      feedName = feed.title || url;
+    } catch (e) {
+      feedName = url;
+    }
+  }
+  subscribeToFeed(url, feedName);
+  res.json({ message: `Subscribed to ${feedName} (${url})` });
 });
 
-// (You can add more API endpoints here)
+import { Feed } from 'feed';
+import fs from 'fs';
+
+// Publish subscriptions as an RSS feed
+apiApp.get('/subscriptions-rss', (req, res) => {
+  // Always read the latest subscriptions from disk
+  let subscriptionsList = [];
+  try {
+    subscriptionsList = JSON.parse(fs.readFileSync('./data/subscriptions.json', 'utf-8'));
+  } catch (e) {}
+
+  const feed = new Feed({
+    title: 'My Subscribed RSS Feeds',
+    description: 'A list of feeds I am subscribed to',
+    id: 'http://localhost:3001/subscriptions-rss',
+    link: 'http://localhost:3001/subscriptions-rss',
+    language: 'en',
+    updated: new Date(),
+    generator: 'airss',
+    feedLinks: { rss2: 'http://localhost:3001/subscriptions-rss' },
+    author: { name: 'RSS Publisher' },
+  });
+
+  subscriptionsList.forEach(sub => {
+    feed.addItem({
+      title: sub.name || sub.url,
+      id: sub.url,
+      link: sub.url,
+      description: `Subscribed feed: ${sub.name || sub.url}`,
+      date: new Date(),
+    });
+  });
+
+  res.type('application/rss+xml');
+  res.set('Content-Type', 'application/rss+xml; charset=utf-8');
+  res.send(feed.rss2());
+});
+
+// Publish articles as an RSS feed, filtered by feedUrl if provided
+apiApp.get('/articles-rss', (req, res) => {
+  let articles = [];
+  try {
+    articles = JSON.parse(fs.readFileSync('./data/articles.json', 'utf-8'));
+  } catch (e) {}
+
+  const { feedUrl } = req.query;
+  let filtered = articles;
+  if (feedUrl) {
+    filtered = articles.filter(a => a.feedUrl === feedUrl);
+  }
+
+  const feed = new Feed({
+    title: feedUrl ? `Articles for ${feedUrl}` : 'All Articles',
+    description: feedUrl ? `RSS articles for ${feedUrl}` : 'All articles from all feeds',
+    id: 'http://localhost:3001/articles-rss',
+    link: 'http://localhost:3001/articles-rss',
+    language: 'en',
+    updated: new Date(),
+    generator: 'airss',
+    feedLinks: { rss2: 'http://localhost:3001/articles-rss' },
+    author: { name: 'RSS Publisher' },
+  });
+
+  filtered.forEach(article => {
+    feed.addItem({
+      title: article.title || article.link,
+      id: article.id || article.link,
+      link: article.link,
+      description: article.summary || '',
+      content: article.content || '',
+      date: article.pubDate ? new Date(article.pubDate) : new Date(),
+    });
+  });
+
+  res.type('application/rss+xml');
+  res.set('Content-Type', 'application/rss+xml; charset=utf-8');
+  res.send(feed.rss2());
+});
 
 apiApp.listen(API_PORT, () => {
   console.log(`API running at http://localhost:${API_PORT}`);
