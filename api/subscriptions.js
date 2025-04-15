7181eda31db2f3e0f4a9c78e9291d2fc4c3ba1a4
Yifan Li <ivan@Yifans-MacBook-Pro.local>
2025-04-14 23:07:51 -0700

    Update subscription logic to save subscriptions to a file

--- api/subscriptions.js
+++ api/subscriptions.js
@@ -1,10 +1,21 @@
 // Subscription logic: in-memory store and subscribeToFeed
 
-const subscriptions = [];
+import fs from 'fs';
+const SUBS_FILE = './data/subscriptions.json';
+
+let subscriptions = [];
+if (fs.existsSync(SUBS_FILE)) {
+  subscriptions = JSON.parse(fs.readFileSync(SUBS_FILE, 'utf-8'));
+}
+
+function saveSubscriptions() {
+  fs.writeFileSync(SUBS_FILE, JSON.stringify(subscriptions, null, 2));
+}
 
 function subscribeToFeed(url) {
   if (!subscriptions.includes(url)) {
     subscriptions.push(url);
+    saveSubscriptions();
     console.log(`Subscribed to: ${url}`);
   } else {
     console.log(`Already subscribed to: ${url}`);
