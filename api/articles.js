576c3f6fab8128bde22486324ee116b27eb32780
Yifan Li <ivan@Yifans-MacBook-Pro.local>
2025-04-28 17:57:00 -0700

    Add function to get articles by feedId and feed by feedId

--- api/articles.js
+++ api/articles.js
@@ -28,3 +28,40 @@ export async function getArticlesByFeedUrl(feedUrl) {
   const db = await connectDB();
   return db.collection('articles').find({ feedUrl }).toArray();
 }
+
+
+// Get articles by feedId
+export async function getArticlesByFeedId(feedId) {
+  const db = await connectDB();
+  let query = { feedId };
+  
+  // Try to convert feedId to ObjectId if it's a valid ObjectId string
+  try {
+    const { ObjectId } = await import('mongodb');
+    if (ObjectId.isValid(feedId)) {
+      query = { feedId: new ObjectId(feedId) };
+    }
+  } catch (error) {
+    console.error('Error converting feedId to ObjectId:', error);
+  }
+  
+  return db.collection('articles').find(query).toArray();
+}
+
+// get feed by feedId
+export async function getFeedById(feedId) {
+  const db = await connectDB();
+  let query = { _id: feedId };
+  
+  // Try to convert feedId to ObjectId if it's a valid ObjectId string
+  try {
+    const { ObjectId } = await import('mongodb');
+    if (ObjectId.isValid(feedId)) {
+      query = { _id: new ObjectId(feedId) };
+    }
+  } catch (error) {
+    console.error('Error converting feedId to ObjectId:', error);
+  }
+  
+  return db.collection('subscriptions').findOne(query);
+}
