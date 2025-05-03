063341bcbf0f8dd4991613ca5a5d8ea299e69509
ivan <ivanlee1999@163.com>
2025-05-03 22:33:28 +0000

    Add functionality to delete a subscription by ID

--- api/subscriptions.js
+++ api/subscriptions.js
@@ -1,6 +1,7 @@
 // Subscription logic: MongoDB store and subscribeToFeed
 
 import { connectDB } from './db.js'; // Uses DB_URI from .env
+import { ObjectId } from 'mongodb';
 
 // Subscribe to a feed (insert if not present)
 export async function subscribeToFeed(url, name) {
@@ -23,4 +24,11 @@ export async function getFeedNameByUrl(url) {
   const db = await connectDB();
   const sub = await db.collection('subscriptions').findOne({ url });
   return sub ? sub.name : url;
+}
+
+// Delete a subscription by ID
+export async function deleteSubscription(id) {
+  const db = await connectDB();
+  const result = await db.collection('subscriptions').deleteOne({ _id: new ObjectId(id) });
+  return result.deletedCount > 0;
 }
